1bafb02289bb7d84888b831b235d38323b8f3c4b
Karol <karolkuczynski5@gmail.com>
2021-12-14 21:48:39 +0100

    tests

---
+++ sportowe_zakapiory_backend/resources/views/test.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.layout')
+
+@section('content')
+<h1>test</h1>
+@stop
+@if(3==3)
+    <h2>tesasdasdt</h2>
+<h3>22</h3>
+@endif
--- sportowe_zakapiory_backend/routes/web.php
+++ sportowe_zakapiory_backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::resource('posts',"\App\Http\Controllers\PostsController");
+Route::get('/view/tests/1{id}', "App\Http\Controllers\PostsController@test");
+Route::get('/databases/tests/insert', function(){
+    DB::insert('insert into posts(title,body) values(?,?)', ['PHP with laravela', 'Laravell ias the best thing that has happened to PHP']);
+});
+Route::get('/databases/tests/select', function(){
+    $result = DB::select('select * from posts');
+    foreach($result as $a) {
+        //return $a->title;
+        echo $a->title . "<br>";
+        echo $a->body . "<br>";
+    }
+});
+Route::get('/databases/tests/update', function() {
+    $updated = DB::update('update posts set title = "upsdated title"');
+    return $updated;
+});
+Route::get('/databases/tests/delete', function(){
+    return $deleted;
+});
+Route::get('/eloquent', function(){
+    $post = Post::all(); 
+    foreach($post as $n) {             
+        echo $n->title . "<br>";
+    } 
+});
+Route::get('/eloquent/find', function(){
+    $post = Post::find(3);
+    return $post->title;
 });
