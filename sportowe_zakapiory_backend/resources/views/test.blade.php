f5ad23c7ad4b910cbd84bf48f3467df01194c2b0
Karol <karolkuczynski5@gmail.com>
2021-12-15 18:00:36 +0100

    temporary register system xd

--- sportowe_zakapiory_backend/resources/views/test.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.layout')
-
-@section('content')
-<h1>test</h1>
-@stop
-@if(3==3)
-    <h2>tesasdasdt</h2>
-<h3>22</h3>
-@endif
--- sportowe_zakapiory_backend/routes/web.php
+++ sportowe_zakapiory_backend/routes/web.php
@@ -46,3 +46,23 @@ Route::get('/eloquent/find', function(){
     $post = Post::find(3);
     return $post->title;
 });
+Route::get('/registerr/{a}/{b}/{c}', function($a,$b,$c) {
+    DB::insert('insert into users(name,email,password) values(?,?,?)', [$a, $b, $c]);
+});
+// temporary login system
+Route::get('/games', '\App\Http\Controllers\GamesController@index');
+
+Route::get('/games/create', '\App\Http\Controllers\GamesController@create');
+
+Route::get('/games/{game}', '\App\Http\Controllers\GamesController@show');
+
+Route::post('/games', '\App\Http\Controllers\GamesController@store');
+
+Route::post('/games/{game}/reviews', '\App\Http\Controllers\ReviewsController@store');
+
+Route::get('/register', '\App\Http\Controllers\RegistrationController@create');
+Route::post('register', '\App\Http\Controllers\RegistrationController@store');
+
+Route::get('/login', '\App\Http\Controllers\SessionsController@create');
+Route::post('/login', '\App\Http\Controllers\SessionsController@store');
+Route::get('/logout', '\App\Http\Controllers\SessionsController@destroy');
